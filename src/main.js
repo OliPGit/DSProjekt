83881b3cfae1c6f501a44ca8bf62fd31e1686599
Oliver Petra <oliver.petra@sap.com>
2021-01-13 23:31:51 +0100

    update

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ jQuery(($) => {
     console.log('Worker');
     const wfWorker = new Worker('src/searchAndReplace.js');
     const wfWMessage = { wMessage: { htmlC: htmlContent } };
+    wfWorker.postMessage(wfWMessage);
   } else {
     console.log('No Worker');
   }
