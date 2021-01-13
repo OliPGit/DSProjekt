62059f57949d5db1d6b3938451a0cc63bbea24ff
Oliver Petra <oliver.petra@sap.com>
2021-01-14 00:14:38 +0100

    update

--- src/searchAndReplace.js
+++ src/searchAndReplace.js
@@ -1,5 +1,5 @@
 this.onmessage = function (e) {
   if (e.data.wMessage !== this.undefined) {
-    console.log(e.data.wMessage.htmlContent);
+    console.log('inside worker: ', e.data.wMessage);
   }
 };
