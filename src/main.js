ff4b432bf20f31a51af813e9be33a5cc1df5d341
Oliver Petra <oliver.petra@sap.com>
2021-01-13 23:29:55 +0100

    update

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ jQuery(($) => {
   const htmlContent = $('#main-content').text();
 
   if (window.Worker) {
+    console.log('Worker');
     const wfWorker = new Worker('src/searchAndReplace.js');
     const wfWMessage = { wMessage: { htmlC: htmlContent } };
   } else {
